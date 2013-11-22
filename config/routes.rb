278b1e33a830905b373e0b5ca63fbcd788b741be
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-11-21 20:33:17 -0800

    Created posts controller with "new" action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Blog::Application.routes.draw do
+
+  resources :posts
+  
   root "welcome#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
