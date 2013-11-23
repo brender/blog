0184827da9d5a929ed93aa596e390635df0ff2eb
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-11-23 13:51:07 -0800

    Created index page view and action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def index
+    @posts = Post.all
+  end
+
   private
   	def post_params
   		params.require(:post).permit(:title, :text)
