f6badbf8ccedf54ad994c2aa092f86083f1ac266
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-11-23 19:58:00 -0800

    Removed welcome everything

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Blog::Application.routes.draw do
     resources :comments
   end
   
-  root "welcome#index"
+  root "posts#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
