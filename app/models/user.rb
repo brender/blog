61c69ada3a9b150f0aaf573b2c1cce8f49b4d3af
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-12-10 19:09:40 -0800

    Disable registration in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  if Rails.env.development?
-    devise :database_authenticatable, :rememberable, :trackable, :validatable
-  else
-    devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
-  end
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
