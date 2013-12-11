4553bb985377171a47601cb69f47760f2a4c7c28
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-12-10 18:28:44 -0800

    Made password not recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   if Rails.env.development?
-    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :rememberable, :trackable, :validatable
   else
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
   end
 end
