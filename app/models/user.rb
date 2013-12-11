7b622cb5fad825ff43b51911cb75d0350716bb6f
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-12-10 18:25:51 -0800

    Disabled sign up in development environment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  if Rails.env.development?
+    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  else
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  end
 end
