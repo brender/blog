0e89efa24f8172b3f4176f85f4aed77e4874140f
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2014-01-05 15:06:46 -0800

    Add thumbnail to view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 1 }
-  attr_accessible :thumbnail
   has_attached_file :thumbnail
 end
