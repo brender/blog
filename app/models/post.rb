2ad35ac2e8a267c4552bd85e3d845617043ccd3a
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2014-01-05 18:14:43 -0800

    Sized old thumbnails with paperclip floated left in css

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 1 }
-  has_attached_file :thumbnail
+  has_attached_file :thumbnail, :styles => { 
+    :thumb => "200x200#"
+  }
 end
