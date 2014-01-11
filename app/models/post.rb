acd91f53f979d240b97a1c0eeade7febf9942c2d
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2014-01-10 20:21:57 -0800

    Thumbnail required for new post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 1 }
+  validates :thumbnail, presence: true
   has_attached_file :thumbnail, :styles => { 
     :thumb => "200x200#"
   }
