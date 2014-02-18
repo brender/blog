695e88c28850902cd455d5f3cb34707408201f9b
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2014-02-17 19:17:22 -0800

    Errors show up for comments
    Fixes #45

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,13 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter,:body))
-    redirect_to post_path(@post)
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      @post.reload
+      render :template => "posts/show"
+    end
   end
 
   def destroy
