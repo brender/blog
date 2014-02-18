50ed100b53d7cab75c49fddbb6b0c1482fe23481
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2014-02-17 19:04:14 -0800

    Empty comments don't appear

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :body, presence: true, length: { minimum: 1 }
+  validates :commenter, presence: true, length: { minimum: 1 }
 end
